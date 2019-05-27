f3c8e470a0075f7668f2b89b108984f7aebcafc7
Shay Zeevi <shay.z@homepls.co.il>
2019-05-27 19:59:45 +0300

    bundling the script to pls-binding.min.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,25 @@
 "use strict";
 let scope = {};
 (function plsDataBinding() {
+    /**
+     * For changing the attribute name from pls-binding to something else you can search replace for pls-binding to your custom name
+     * @type {NodeListOf<Element>}
+     */
     let bindingElements = document.querySelectorAll('[pls-binding]'); // get all the elements with the pls-binding attribute
     bindingElements.forEach((element) => { // Iterate all the elements in the document with attribute pls-binding
         let elementAttributeValue = element.getAttribute('pls-binding'); // Get the value of the attribute
         addToScope(elementAttributeValue);
         if (element.type == 'text') {
-            element.onkeyup = () => {
+            element.onkeyup = () => { // Set the binding for keyup
                 scope[elementAttributeValue] = element.value;
             }
         }
     });
 
+    /**
+     * This function will take the value of the input and set it to the html element with the same attribute value
+     * @param elementAttributeValue
+     */
     function addToScope(elementAttributeValue) {
         if (!scope.hasOwnProperty(elementAttributeValue)) { // check if scope has the elementAttribute
             let value;
